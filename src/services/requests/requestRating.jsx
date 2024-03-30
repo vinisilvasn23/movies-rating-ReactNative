ed1743f8f4552b3ccbc5511dd8ca7230024cf7d3
Vinicius <vini456nascimento@gmail.com>
2024-03-30 07:36:23 -0300

    feat: created rating requests

---
+++ src/services/requests/requestRating.jsx
@@ -0,0 +1,30 @@
+import { api } from "../api";
+
+const handleRequest = async (method, url, token) => {
+  try {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+
+    const response = await api[method](url, config);
+
+    return response.data;
+  } catch (error) {
+    if (error.response && error.response.data) {
+      console.error(`Erro na requisição ${method} para ${url}:`, error.response.data);
+    } else {
+      console.error(`Erro na requisição ${method} para ${url}:`, error);
+    }
+    throw error;
+  }
+};
+
+export const listRatings = async (token) => {
+  return handleRequest("get", "/ratings/", token);
+};
+
+export const getRatingDetail = async (token, ratingId) => {
+  return handleRequest("get", `/ratings/${ratingId}/`, token);
+};
