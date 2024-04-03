6ab2eebca10f5ca7f263da00874282d028e3f820
Vinicius <vini456nascimento@gmail.com>
2024-04-03 08:23:19 -0300

    feat: updated homepage

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,8 +1,14 @@
 import React, { useEffect, useState } from "react";
-import { Text, View } from "react-native";
-import Login from "../../components/login";
 import Loading from "../../components/loading";
 import { useUser } from "../../context/userContext";
+import Dashboard from "../dashboard";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import LoginForm from "../../components/login";
+import MovieDetail from "../../components/movieDetail";
+import RegisterForm from "../../components/register";
+
+const Stack = createStackNavigator();
 
 const Home = () => {
   const [isLoading, setIsLoading] = useState(true);
@@ -19,9 +25,37 @@ const Home = () => {
   if (isLoading) return <Loading />;
 
   return (
-    <View>
-      {isUserLoggedIn ? <Text>O usuário está logado!</Text> : <Login />}
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        {isUserLoggedIn ? (
+          <>
+            <Stack.Screen
+              name="Dashboard"
+              component={Dashboard}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="MovieDetail"
+              component={MovieDetail}
+              options={{ headerShown: false }}
+            />
+          </>
+        ) : (
+          <>
+            <Stack.Screen
+              name="Login"
+              component={LoginForm}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="Register"
+              component={RegisterForm}
+              options={{ headerShown: false }}
+            />
+          </>
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
