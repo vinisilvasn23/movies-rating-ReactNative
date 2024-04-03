efea37c1310639b384d4603f02845eb65a324b9b
Vinicius <vini456nascimento@gmail.com>
2024-04-03 08:17:22 -0300

    feat: update app

--- App.js
+++ App.js
@@ -1,15 +1,14 @@
 import React from "react";
-import { View } from "react-native";
-import tw from "tailwind-react-native-classnames";
-import RoutesMain from "./src/routes/routesMain";
 import { UserProvider } from "./src/context/userContext";
+import { MovieProvider } from "./src/context/movieContext";
+import Routes from "./src/routes/routes";
 
 const App = () => {
   return (
     <UserProvider>
-      <View style={[tw`flex-1 justify-center items-center`, { transitionDuration: 500 }]}>
-        <RoutesMain />
-      </View>
+      <MovieProvider>
+          <Routes />
+      </MovieProvider>
     </UserProvider>
   );
 };
