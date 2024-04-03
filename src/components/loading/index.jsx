2a83bb997bdde45968123350743b4c8bea882c39
Vinicius <vini456nascimento@gmail.com>
2024-04-03 08:19:52 -0300

    fix: component loading

--- src/components/loading/index.jsx
+++ src/components/loading/index.jsx
@@ -1,25 +1,24 @@
-import React from 'react';
-import { View, Image } from 'react-native';
-import Spinner from 'react-native-loading-spinner-overlay';
-import tw from 'tailwind-react-native-classnames';
+import React from "react";
+import { View, Image } from "react-native";
+import Spinner from "react-native-loading-spinner-overlay";
 
 const Loading = () => {
   return (
-    <View style={tw`flex-1 justify-center items-center`}>
-      <View style={tw`flex items-center mb-8`}>
-        <View style={tw`flex items-center`}>
+    <View className="flex-1 justify-center items-center">
+      <View className="flex items-center mb-8">
+        <View className="flex items-center">
           <Image
             source={require("../../../assets/logo.png")}
             style={{ width: 120, height: 120 }}
           />
         </View>
+      </View>
       <Spinner
         visible={true}
-        textContent={'Carregando...'}
-        textStyle={tw`text-lg text-gray-700`}
-        overlayColor={'rgba(0, 0, 0, 0.5)'}
+        textContent={"Carregando..."}
+        textClassName={"text-lg text-gray-700"}
+        overlayColor={"rgba(0, 0, 0, 0.5)"}
       />
-      </View>
     </View>
   );
 };
