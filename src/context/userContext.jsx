ceffc1a96055bcf7ba96124b5384901b1f8c8396
Vinicius <vini456nascimento@gmail.com>
2024-04-04 13:37:09 -0300

    fix: corrections in the code and implemented asyncstorage colorscheme

--- src/context/userContext.jsx
+++ src/context/userContext.jsx
@@ -1,6 +1,8 @@
 import React, { createContext, useContext, useEffect, useState } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { getUserById } from "../services/requests/requestsUser";
+import { createUserRequest, deleteUser, getUserById, loginUser } from "../services/requests/requestsUser";
+import { useColorScheme } from "nativewind";
+import { ToastAndroid } from "react-native";
 
 const UserContext = createContext();
 
@@ -13,11 +15,15 @@ export const UserProvider = ({ children }) => {
   const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
   const [user, setUser] = useState(null);
   const [tokenUser, setToken] = useState(null);
+  const colorScheme = useColorScheme();
 
   useEffect(() => {
     const checkLoggedInStatus = async () => {
       setIsLoading(true);
       try {
+        const theme = await AsyncStorage.getItem("colorTheme");
+        colorScheme.setColorScheme(theme);
+
         const token = await AsyncStorage.getItem("userToken");
         const userId = await AsyncStorage.getItem("userId");
 
@@ -33,7 +39,7 @@ export const UserProvider = ({ children }) => {
           }
         }
       } catch (error) {
-        console.error("Erro ao verificar o status de login:", error);
+        ToastAndroid.show("Token expirado. Redirecionando para a tela de login...", ToastAndroid.SHORT);
       } finally {
         setIsLoading(false);
       }
@@ -42,9 +48,30 @@ export const UserProvider = ({ children }) => {
     checkLoggedInStatus();
   }, [isUserLoggedIn, tokenUser]);
 
-  const logout = async () => {
+  const login = async (data) => {
+    setIsLoading(true);
     try {
-      setIsLoading(true);
+      const response = await loginUser(data);
+      const token = response.access;
+      const userId = response.user_id;
+      await AsyncStorage.setItem("userToken", token);
+      await AsyncStorage.setItem("userId", userId);
+      setIsUserLoggedIn(true);
+      setToken(token);
+      const user = await getUserById(token, userId);
+      setUser(user);
+      return true;
+    } catch (error) {
+      console.error("Erro ao fazer login:", error);
+      return false;
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const logout = async () => {
+    setIsLoading(true);
+    try {
       await AsyncStorage.removeItem("userToken");
       await AsyncStorage.removeItem("userId");
       setIsUserLoggedIn(false);
@@ -57,15 +84,39 @@ export const UserProvider = ({ children }) => {
     }
   };
 
+  const createUser = async (userData) => {
+    setIsLoading(true);
+    try {
+      await createUserRequest(userData);
+    } catch (error) {
+      throw error;
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const confirmDeleteAccount = async () => {
+    try {
+      await deleteUser(tokenUser, user.id);
+      await AsyncStorage.removeItem("userToken");
+      await AsyncStorage.removeItem("userId");
+      setIsUserLoggedIn(false)
+    } catch (error) {
+      console.error("Erro ao excluir a conta:", error);
+    }
+  };
+
   return (
     <UserContext.Provider
       value={{
         isLoading,
         isUserLoggedIn,
-        setIsUserLoggedIn,
         user,
         tokenUser,
+        login,
         logout,
+        createUser,
+        confirmDeleteAccount
       }}
     >
       {children}
