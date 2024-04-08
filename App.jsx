a2408b0744ab5958e73c5e78d135df6684dc6e6c
Vinicius <vini456nascimento@gmail.com>
2024-04-08 08:03:02 -0300

    feat: implemented AppWraper

--- App.jsx
+++ App.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import { UserProvider } from "./src/context/userContext";
 import { MovieProvider } from "./src/context/movieContext";
 import Routes from "./src/routes/routes";
+import "./global.css";
+import AppWrapper from "./src/components/appWrapper";
 
 const App = () => {
   return (
     <UserProvider>
       <MovieProvider>
+        <AppWrapper>
           <Routes />
+        </AppWrapper>
       </MovieProvider>
     </UserProvider>
   );
