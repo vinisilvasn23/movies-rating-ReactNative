41c7b88ef6191602013c53840f32a81afbad0a6d
Vinicius <vini456nascimento@gmail.com>
2024-04-08 08:06:08 -0300

    fix: filter movie and style

--- src/components/searchResults/index.jsx
+++ src/components/searchResults/index.jsx
@@ -1,17 +1,17 @@
+import { Ionicons } from "@expo/vector-icons";
+import { useNavigation, useRoute } from "@react-navigation/native";
 import React, { useEffect, useState } from "react";
 import {
-  View,
-  Text,
-  ScrollView,
-  TouchableOpacity,
-  Image,
   ActivityIndicator,
+  Image,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
 } from "react-native";
-import { useRoute, useNavigation } from "@react-navigation/native";
-import { findMovies, findSeries } from "../../services/requests/requestMovies";
-import { Ionicons } from "@expo/vector-icons";
 import { statusBarHeight } from "../../config";
 import { useMovie } from "../../context/movieContext";
+import { findMovies, findSeries } from "../../services/requests/requestMovies";
 
 const SearchResults = () => {
   const [movies, setMovies] = useState([]);
@@ -46,7 +46,12 @@ const SearchResults = () => {
 
   const loadMovies = async () => {
     const newMovies = await fetchResults();
-    setMovies((prevMovies) => [...prevMovies, ...newMovies]);
+    setMovies((prevMovies) => {
+      const uniqueNewMovies = newMovies.filter(
+        (movie) => !prevMovies.some((prevMovie) => prevMovie.id === movie.id)
+      );
+      return [...prevMovies, ...uniqueNewMovies];
+    });
     setPage((prevPage) => prevPage + 1);
   };
 
@@ -66,11 +71,15 @@ const SearchResults = () => {
   };
 
   return (
-    <ScrollView className="dark:bg-slate-800" onScroll={handleScroll}>
-      <View style={{ paddingTop: statusBarHeight }}>
+    <ScrollView
+      style={{ paddingTop: statusBarHeight }}
+      className="dark:bg-slate-900"
+      onScroll={handleScroll}
+    >
+      <View>
         <TouchableOpacity
           onPress={() => navigation.goBack()}
-          className="absolute top-12 left-4 z-10"
+          className="absolute top-4 left-4 z-10"
           style={{ zIndex: 1 }}
         >
           <View className="bg-black p-2 rounded-full">
@@ -78,7 +87,7 @@ const SearchResults = () => {
           </View>
         </TouchableOpacity>
       </View>
-      <View className="p-4 pt-20">
+      <View className="p-4 pt-16">
         <Text className="text-xl font-bold mb-4 text-center dark:text-white">
           Resultados da pesquisa para "{query}"
         </Text>
@@ -89,20 +98,24 @@ const SearchResults = () => {
               className="mb-4 mr-4"
               onPress={() => goToMovieDetail(movie)}
             >
-              <View className="flex-col">
-                <Image
-                  source={{
-                    uri: `https://image.tmdb.org/t/p/w500/${movie.poster_path}`,
-                  }}
-                  style={{ width: 120, height: 180, borderRadius: 8 }}
-                />
-                <Text
-                  className="text-sm dark:text-white"
-                  style={{ maxWidth: 80 }}
-                  numberOfLines={2}
-                >
-                  {movie.title ? movie.title : movie.name}
-                </Text>
+              <View className="flex-col pb-4">
+                {movie.poster_path && (
+                  <>
+                    <Image
+                      source={{
+                        uri: `https://image.tmdb.org/t/p/w500/${movie.poster_path}`,
+                      }}
+                      style={{ width: 120, height: 180, borderRadius: 8 }}
+                    />
+                    <Text
+                      className="text-sm dark:text-white"
+                      style={{ maxWidth: 80 }}
+                      numberOfLines={2}
+                    >
+                      {movie.title ? movie.title : movie.name}
+                    </Text>
+                  </>
+                )}
               </View>
             </TouchableOpacity>
           ))}
